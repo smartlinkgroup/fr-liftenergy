18a0f32e1f373f4563ce7fa0dc4d35d563a4309f
Alonsou29 <alonsourdaneta.29@hotmail.com>
2025-11-19 16:23:45 -0400

    add modal

---
+++ src/app/modals/information-modal/information-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-modal',
+  imports: [],
+  templateUrl: './information-modal.component.html',
+  styleUrl: './information-modal.component.css'
+})
+export class InformationModalComponent {
+
+}
